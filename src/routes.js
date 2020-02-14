5831e66546a9f6a3bcce60357b287d2ad155df72
NaluFigueira <anachfigueira@gmail.com>
2020-02-14 00:06:01 -0300

    Configurando rotas inciais e Header Bar

---
+++ src/routes.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Sign from '~/pages/Sign';
+import Checkins from '~/pages/Checkins';
+import HelpOrders from '~/pages/HelpOrders';
+import Answers from '~/pages/Answers';
+import NewHelpOrder from '~/pages/NewHelpOrder';
+
+const Stack = createStackNavigator();
+
+function LogoTitle() {
+  return (
+    <Image
+      resizeMode="contain"
+      style={{ height: '80%' }}
+      source={require('~/assets/sideLogo.png')}
+    />
+  );
+}
+
+function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Sign"
+        screenOptions={{
+          headerTitleAlign: 'center',
+          headerTitle: props => <LogoTitle {...props} />,
+        }}>
+        <Stack.Screen name="Sign" component={Sign} options={{ header: null }} />
+        <Stack.Screen name="Checkins" component={Checkins} />
+        <Stack.Screen name="HelpOrders" component={HelpOrders} />
+        <Stack.Screen name="Answers" component={Answers} />
+        <Stack.Screen name="NewHelpOrder " component={NewHelpOrder} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Routes;
