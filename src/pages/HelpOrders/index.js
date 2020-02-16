2c780006dcd79ed7cb338516a7fa380a988e83f5
NaluFigueira <anachfigueira@gmail.com>
2020-02-15 22:24:14 -0300

    Adicionando tela de listagem de pedidos de auxílio

--- src/pages/HelpOrders/index.js
+++ src/pages/HelpOrders/index.js
@@ -1,8 +1,109 @@
-import React from 'react';
-import { View } from 'react-native';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import PropTypes from 'prop-types';
 
-// import { Container } from './styles';
+import { formatRelative, parseISO } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+
+import Button from '~/components/Button';
+import {
+  Container,
+  HelpOrderCardList,
+  HelpOrderCard,
+  HelpOrderInfo,
+  HelpOrderStatusContainer,
+  HelpOrderStatus,
+  HelpOrderDate,
+  HelpOrderQuestion,
+} from './styles';
 
 export default function HelpOrders() {
-  return <View />;
+  const [checkins, setCheckIns] = useState([
+    {
+      id: 3,
+      student_id: 4,
+      question:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      answer: 'Resposta',
+      answer_at: '2019-12-29T20:29:11.303Z',
+      createdAt: '2019-12-29T20:26:53.083Z',
+      updatedAt: '2019-12-29T20:29:11.304Z',
+    },
+    {
+      id: 4,
+      student_id: 4,
+      question: 'Deve fazer alguma dieta?',
+      answer: null,
+      answer_at: '2019-12-29T20:29:11.303Z',
+      createdAt: '2019-12-29T20:26:53.083Z',
+      updatedAt: '2019-12-29T20:29:11.304Z',
+    },
+  ]);
+
+  function formatCheckinDate(checkinDate) {
+    let formattedDate = checkinDate.split('.');
+    formattedDate = formattedDate[0].replace('T', ' ');
+    formattedDate = formatRelative(parseISO(formattedDate), new Date(), {
+      locale: ptBR,
+    });
+    return formattedDate;
+  }
+
+  useEffect(() => {
+    setCheckIns(
+      checkins.map(checkin => {
+        return { ...checkin, createdAt: formatCheckinDate(checkin.createdAt) };
+      }),
+    );
+  }, []);
+
+  const renderItem = ({ item }) => {
+    return (
+      <HelpOrderCard>
+        <HelpOrderInfo>
+          <HelpOrderStatusContainer>
+            <Icon
+              name={item.answer ? 'check' : 'sms'}
+              size={20}
+              style={{ marginRight: 10 }}
+              color={item.answer ? '#42CB59' : '#666'}
+            />
+            <HelpOrderStatus>
+              {item.answer ? 'Respondido' : 'Sem Resposta'}
+            </HelpOrderStatus>
+          </HelpOrderStatusContainer>
+          <HelpOrderDate>{item.createdAt}</HelpOrderDate>
+        </HelpOrderInfo>
+        <HelpOrderQuestion>{item.question}</HelpOrderQuestion>
+      </HelpOrderCard>
+    );
+  };
+
+  return (
+    <Container>
+      <Button text="Novo pedido de auxílio" />
+      <HelpOrderCardList
+        data={checkins}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+      />
+    </Container>
+  );
 }
+
+HelpOrders.defaultProps = {
+  item: {},
+};
+
+HelpOrders.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.number,
+    student_id: PropTypes.number,
+    question: PropTypes.string,
+    answer: PropTypes.string,
+    answer_at: PropTypes.string,
+    createdAt: PropTypes.string,
+    updatedAt: PropTypes.string,
+  }),
+};
--- src/pages/HelpOrders/styles.js
+++ src/pages/HelpOrders/styles.js
@@ -1,3 +1,52 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  padding: 0 15px;
+  background-color: #fff;
+  margin-top: 10%;
+`;
+
+export const HelpOrderCardList = styled(FlatList)`
+  margin-top: 5%;
+`;
+
+export const HelpOrderCard = styled.View`
+  padding: 10px 15px;
+  border: 2px solid #ddd;
+  border-radius: 4px;
+  flex-direction: column;
+  margin-bottom: 10px;
+  max-height: 150px;
+`;
+
+export const HelpOrderInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 0px;
+`;
+
+export const HelpOrderStatusContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const HelpOrderStatus = styled.Text`
+  font-weight: bold;
+  font-size: 14px;
+  color: ${props => (props.answered ? '#42CB59' : '#666')};
+`;
+
+export const HelpOrderDate = styled.Text`
+  color: #666;
+`;
+
+export const HelpOrderQuestion = styled.Text.attrs({
+  numberOfLines: 3,
+})`
+  font-size: 14px;
+  color: #666;
+  padding: 0px 10px 0px 0px;
+`;
